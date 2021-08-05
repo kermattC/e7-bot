e313659411c6240f0f5c36171035ec3f7e4d6113
unknown <matthew.chan5@ontariotechu.net>
2021-08-05 13:19:43 -0400

    Modified random message function

--- main.py
+++ main.py
@@ -27,21 +27,25 @@ async def on_message(message): # called when message is received.
 # event when new user joins
 @client.event
 async def on_member_join(member):
-  # generate random intenger from 0 to 4
-  rand_greeting = random.randrange(0, 5)
-  channel = discord.utils.get(member.guild.text_channels, name="hello")
   # get the member's mention
   mention = member.mention
+  greetings = [f"Welcome to our discord server {mention}! Please change your nickname to your e7 name so everyone knows who you are! Feel free to chat about anything", f"Do you hear the approaching ruin {mention}? Just kidding! Welcome to the server and please change your nickname to your e7 name so we know who you are!", f"Glad you made it {mention}!  Please change your nickname to your e7 name so we know who you are!", f"Hello and welcome to the discord {mention}! Please change your nickname to your e7 name so we know who you are!"]
+  # generate random intenger from 0 to 4
+  random_greeting = random.choice(greetings)
+  # rand_greeting = random.randrange(0, 5)
+  channel = discord.utils.get(member.guild.text_channels, name="hello")
+  await channel.send(random_greetings)
+
 
   # no switch case here so we resort to if/else if
-  if rand_greeting == 0:
-    await channel.send(f"Welcome to our discord server {mention}! Please change your nickname to your e7 name so everyone knows who you are! Feel free to chat about anything")
-  elif rand_greeting == 1:
-    await channel.send(f"Do you hear the approaching abyss {mention}? Just kidding! Welcome to the server and please change your nickname to your e7 name so we know who you are!")
-  elif rand_greeting == 2:
-    await channel.send(f"Glad you made it {mention}!  Please change your nickname to your e7 name so we know who you are!")
-  elif rand_greeting == 3:
-    await channel.send(f"Hello and welcome to the discord {mention}! Please change your nickname to your e7 name so we know who you are!")
+  # if rand_greeting == 0:
+  #   await channel.send(f"Welcome to our discord server {mention}! Please change your nickname to your e7 name so everyone knows who you are! Feel free to chat about anything")
+  # elif rand_greeting == 1:
+  #   await channel.send(f"Do you hear the approaching ruin {mention}? Just kidding! Welcome to the server and please change your nickname to your e7 name so we know who you are!")
+  # elif rand_greeting == 2:
+  #   await channel.send(f"Glad you made it {mention}!  Please change your nickname to your e7 name so we know who you are!")
+  # elif rand_greeting == 3:
+  #   await channel.send(f"Hello and welcome to the discord {mention}! Please change your nickname to your e7 name so we know who you are!")
 
 # lastly, run the bot
 token = os.environ['TOKEN']
