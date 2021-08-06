707bde34544c6ea264f88f3960b85da90d7b1751
unknown <matthew.chan5@ontariotechu.net>
2021-08-06 04:52:41 -0400

    Added commands but not working

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
 import os
-import discord
 import random
 import json
+import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='.', intents=intents)
-
+bot = commands.Bot(command_prefix=".", intents=intents)
 # create instance of client to connect to discord. Also apply the intents test
-client = discord.Client(intents = intents)
+# client = discord.Client(intents = intents)
 
 # read json speed data
 speed_json_file = open('resources/speed_data.json', 'r')
 speed_json_data = speed_json_file.read()
 
-# parse
+# parse json data
 speed_data = json.loads(speed_json_data)
 
+@bot.command(name="test")
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@bot.command()
+async def speed(ctx, arg):
+  print("received speed command")
+  name = str(speed_data['Schuri'])
+  base_speed = str(speed_data['speed'][0])
+  set_bonus = str(speed_data['speed'][1])
+  await ctx.send("i am speed. have some speed data bitch - Name: ", name, " Base speed: ", base_speed, " Set bonus: ", set_bonus)
+
 # create client event decorator to register an event
-@client.event
+@bot.event
 # called when bot is ready
 async def on_ready():
-  print("Bot logged in as {0.user}".format(client))
+  print("Bot logged in as {0.user}".format(bot))
 
 # main listening function for commands
-@client.event
+@bot.event
 async def on_message(message): # called when message is received. 
   # however we don't want it to work when the message is from the bot itself
-  if message.author == client.user:
+  if message.author == bot.user:
     return
   if message.content.startswith('.hello'):
     await message.channel.send('Hey there')
 
 # event when new user joins
-@client.event
+@bot.event
 async def on_member_join(member):
   # get the member's mention
   mention = member.mention
@@ -45,14 +60,7 @@ async def on_member_join(member):
   channel = discord.utils.get(member.guild.text_channels, name="hello")
   await channel.send(random_greeting)
 
-@bot.command(name="speed")
-async def get_speed(message):
-  print("received command")
-  name = str(speed_data['Schuri'])
-  base_speed = str(speed_data['speed'][0])
-  set_bonus = str(speed_data['speed'][1])
-  await message.channel.send("i am speed. have some speed data bitch - Name: ", name, " Base speed: ", base_speed, " Set bonus: ", set_bonus)
-  await message.channel.send("Oh yeah here's your message: ", message)
+
 # lastly, run the bot
 token = os.environ['TOKEN']
-client.run(token)
+bot.run(token)
